2b693b78d21e80f19e3bbbfbbaef5cd6a2c5c5f3
muskan572 <muskanchhimpa2022@gmail.com>
2025-03-09 19:35:10 +0530

    complete homepage with some css styling and alignments

--- src/modules/home/home-page.js
+++ src/modules/home/home-page.js
@@ -1,25 +1,47 @@
-import { Avatar, Container, Grid2, Typography } from "@mui/material";
+import { Box, Container, Grid2, Typography } from "@mui/material";
 
 const Homepage = () => {
   return (
     <>
-      <Container
-        maxWidth="lg"
-        sx={{ mt: 8, display: "flex", alignItems: "center" }}
-      >
+      <Container sx={{ mt: 8, display: "flex", alignItems: "center" }}>
         <Grid2 container spacing={4} alignItems="center">
           {/* Profile Image */}
-          <Grid2 size xs={12} md={5}>
-            <Avatar
-              src="https://via.placeholder.com/250"
-              sx={{ width: 250, height: 250, borderRadius: "50%", mx: "auto" }}
-            />
+          <Grid2 size={6}>
+            <Box
+              width="270px"
+              height="400px"
+              borderRadius="150px"
+              borderColor="#E3DFB0"
+              border={1}
+            >
+              <Box
+                width="250px"
+                height="390px"
+                borderRadius="150px"
+                bgcolor="#E3DFB0"
+                borderColor="#E3DFB0"
+                marginLeft={3}
+              >
+                <Box>
+                  <img
+                    src="/assets/image/image.png"
+                    alt="profile "
+                    style={{
+                      width: "250px",
+                      height: "350px",
+                      borderRadius: "120px",
+                      marginTop: "20px",
+                    }}
+                  />
+                </Box>
+              </Box>
+            </Box>
           </Grid2>
 
           {/* Intro Text */}
-          <Grid2 size xs={12} md={7}>
+          <Grid2 size={6}>
             <Typography
-              variant="h4"
+              variant="h1"
               sx={{
                 fontFamily: "cursive",
                 fontWeight: "bold",
@@ -29,12 +51,12 @@ const Homepage = () => {
               Hey, There ...!!
             </Typography>
             <Typography
-              variant="h5"
+              variant="h3"
               sx={{ mt: 1, fontWeight: "bold", color: "#333" }}
             >
               I'm Muskan Jhedu
             </Typography>
-            <Typography variant="body1" sx={{ mt: 2, color: "#555" }}>
+            <Typography sx={{ mt: 2, color: "#555", fontSize: "25px" }}>
               Passionate Front-end Developer with a keen eye for design,
               blending creativity with technical expertise to build intuitive
               and visually engaging user interfaces.
--- src/modules/nav/navbars.js
+++ src/modules/nav/navbars.js
@@ -1,6 +1,7 @@
 import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material";
 import { Instagram, Facebook, LinkedIn } from "@mui/icons-material";
 import { Homepage } from "../home";
+import Person2Icon from "@mui/icons-material/Person2";
 const Navbars = () => {
   return (
     <>
@@ -13,14 +14,23 @@ const Navbars = () => {
             variant="h3"
             sx={{ flexGrow: 1, color: "#4D3C2D", fontWeight: "bold" }}
           >
+            <Person2Icon />
             Muskan
           </Typography>
-          <Stack direction="row">
-            <Button color="inherit">Home</Button>
-            <Button color="inherit">About Me</Button>
-            <Button color="inherit">Education</Button>
-            <Button color="inherit">Skills</Button>
-            <Button color="inherit">Contact</Button>
+          <Stack direction="row" gap={7} justifyContent="center" flex={5}>
+            <Button style={{ color: "#2F1B1B", fontSize: "18px" }}>Home</Button>
+            <Button style={{ color: "#2F1B1B", fontSize: "18px" }}>
+              About Me
+            </Button>
+            <Button style={{ color: "#2F1B1B", fontSize: "18px" }}>
+              Education
+            </Button>
+            <Button style={{ color: "#2F1B1B", fontSize: "18px" }}>
+              Skills
+            </Button>
+            <Button style={{ color: "#2F1B1B", fontSize: "18px" }}>
+              Contact
+            </Button>
           </Stack>
           <Stack direction="row">
             <Button>
